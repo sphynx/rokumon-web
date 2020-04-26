73b36d082e981883eef88cad635a1b14d5b1f9e5
Ivan Veselov <veselov@gmail.com>
2020-04-26 15:02:39 +0100

    Generate seven random cards in Bricks7 layout

--- src/index.js
+++ src/index.js
@@ -2,39 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
+class Card extends React.Component {
+  render() {
+    const kind = this.props.kind;
+    const cls = kind === 'j' ? 'jade' : (kind === 'g' ? 'gold' : 'card');
+    return (
+      <div className={`card ${cls}`}>
+        {this.props.ix}
+      </div>
+    );
+  }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return (<Square
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
-    />);
+
+  constructor(props) {
+    super(props);
+
+    const cards_spec = "jjjgggg";
+    let cards = [];
+    for (const c of cards_spec) {
+      cards.push(c);
+    }
+
+    shuffle(cards);
+
+    this.state = {
+      deck: cards,
+    }
+  }
+
+  renderCard(i) {
+    return <Card ix={i} kind={this.state.deck[i]}/>;
   }
 
   render() {
+    const status = 'Game is about to start';
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        <div className="status">{status}</div>
+        <div className="top-row">
+          {this.renderCard(0)}
+          {this.renderCard(1)}
+          {this.renderCard(2)}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        <div className="bottom-row">
+          {this.renderCard(3)}
+          {this.renderCard(4)}
+          {this.renderCard(5)}
+          {this.renderCard(6)}
         </div>
       </div>
     );
@@ -42,68 +59,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      xIsNext: true,
-      step: 0,
-    }
-  }
-
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.step + 1);
-    const squares = history[history.length - 1].squares.slice();
-    if (squares[i] || calculateWinner(squares)) return;
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{ squares: squares }]),
-      xIsNext: !this.state.xIsNext,
-      step: history.length,
-    });
-  }
-
-  gotoMove(i) {
-    this.setState({
-      step: i,
-      xIsNext: (i % 2) === 0
-    });
-  }
-
   render() {
-    const history = this.state.history;
-    const current = history[this.state.step];
-    const winner = calculateWinner(current.squares);
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    };
-
-    const moves = this.state.history.map((move, ix) => {
-      const desc = ix ? 'Goto move #' + ix : 'Go to game start';
-      return (
-        <li key={ix}>
-          <button onClick={() => this.gotoMove(ix)}>{desc}</button>
-        </li>
-      );
-    });
-
     return (
       <div className="game">
         <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
+          <Board />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
@@ -111,27 +75,17 @@ class Game extends React.Component {
 }
 
 // ========================================
+
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+function shuffle(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
   }
-  return null;
 }
