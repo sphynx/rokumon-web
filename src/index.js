9381b2703c87f958d4564806c7c4d5bd853c9007
Ivan Veselov <veselov@gmail.com>
2020-04-26 16:50:57 +0100

    Add dice

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Card extends React.Component {
-  render() {
-    const kind = this.props.kind;
-    const cls = kind === 'j' ? 'jade' : (kind === 'g' ? 'gold' : 'card');
-    return (
-      <div className={`card ${cls}`}>
-        {this.props.ix}
-      </div>
-    );
-  }
+function Card(props) {
+  const kind = props.kind;
+  const kindClass = kind === 'j' ? 'jade' : (kind === 'g' ? 'gold' : 'card');
+  return (
+    <div className={`card ${kindClass}`}>
+      {props.ix}
+    </div>
+  )
+}
+
+function Die(props) {
+  const die_char = "⚀⚁⚂⚃⚄⚅"[props.value - 1];
+  return (
+    <span className={`die ${props.color + '-die'}`}>
+      {die_char}
+    </span>
+  )
+}
+
+function DiceStock(props) {
+  return (
+    <div className="dice-stock">
+      {props.dice.map((d, ix) =>
+        <Die key={ix} color={d.color} value={d.value} />
+      )}
+    </div>
+  )
 }
 
 class Board extends React.Component {
@@ -33,15 +50,12 @@ class Board extends React.Component {
   }
 
   renderCard(i) {
-    return <Card ix={i} kind={this.state.deck[i]}/>;
+    return <Card ix={i} kind={this.state.deck[i]} />;
   }
 
   render() {
-    const status = 'Game is about to start';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="top-row">
           {this.renderCard(0)}
           {this.renderCard(1)}
@@ -59,12 +73,33 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player1_dice: [
+        { color: 'red', value: 2 },
+        { color: 'red', value: 2 },
+        { color: 'red', value: 4 },
+        { color: 'red', value: 6 },
+      ],
+      player2_dice: [
+        { color: 'black', value: 1 },
+        { color: 'black', value: 1 },
+        { color: 'black', value: 3 },
+        { color: 'black', value: 5 },
+        { color: 'white', value: 1 },
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="game">
+        <DiceStock dice={this.state.player1_dice} />
         <div className="game-board">
           <Board />
         </div>
+        <DiceStock dice={this.state.player2_dice} />
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
@@ -83,9 +118,9 @@ ReactDOM.render(
 
 function shuffle(array) {
   for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
